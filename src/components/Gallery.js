b9014b0a36491ba88e8a43b2d76d09c954b2f461
Ian <ianlloydwilkinson@gmail.com>
2021-03-01 18:59:19 -0700

    gallery updated, link to contact added

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,19 +1,104 @@
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
+import slide1 from "../icons/slideshow2/Knot 2 Rustic slide show 2-01.png"
+import slide2 from "../icons/slideshow2/Knot 2 Rustic slide show 2-02.png"
+import slide3 from "../icons/slideshow2/Knot 2 Rustic slide show 2-03.png"
+import slide4 from "../icons/slideshow2/Knot 2 Rustic slide show 2-04.png"
+import slide5 from "../icons/slideshow2/Knot 2 Rustic slide show 2-05.png"
+import slide6 from "../icons/slideshow2/Knot 2 Rustic slide show 2-06.png"
+import slide7 from "../icons/slideshow2/Knot 2 Rustic slide show 2-07.png"
+import slide8 from "../icons/slideshow2/Knot 2 Rustic slide show 2-08.png"
+import slide9 from "../icons/slideshow2/Knot 2 Rustic slide show 2-09.png"
+import slide10 from "../icons/slideshow2/Knot 2 Rustic slide show 2-10.png"
+import slide11 from "../icons/slideshow2/Knot 2 Rustic slide show 2-11.png"
+import slide12 from "../icons/slideshow2/Knot 2 Rustic slide show 2-12.png"
+import slide13 from "../icons/slideshow2/Knot 2 Rustic slide show 2-13.png"
+import slide14 from "../icons/slideshow2/Knot 2 Rustic slide show 2-14.png"
+import slide15 from "../icons/slideshow2/Knot 2 Rustic slide show 2-15.png"
+import slide16 from "../icons/slideshow2/Knot 2 Rustic slide show 2-16.png"
+import slide17 from "../icons/slideshow2/Knot 2 Rustic slide show 2-17.png"
+import slide18 from "../icons/slideshow2/Knot 2 Rustic slide show 2-18.png"
+import slide19 from "../icons/slideshow2/Knot 2 Rustic slide show 2-19.png"
+
 
 const images = [
   {
-    original: 'https://picsum.photos/id/1018/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    original: slide1,
+    thumbnail: slide1,
   },
   {
-    original: 'https://picsum.photos/id/1015/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    original: slide2,
+    thumbnail: slide2,
   },
   {
-    original: 'https://picsum.photos/id/1019/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    original: slide3,
+    thumbnail: slide3,
   },
+  {
+    original: slide4,
+    thumbnail: slide4,
+  },
+  {
+    original: slide5,
+    thumbnail: slide5,
+  },
+  {
+    original: slide6,
+    thumbnail: slide6,
+  },
+  {
+    original: slide7,
+    thumbnail: slide7,
+  },
+  {
+    original: slide8,
+    thumbnail: slide8,
+  },
+  {
+    original: slide9,
+    thumbnail: slide9,
+  },
+  {
+    original: slide10,
+    thumbnail: slide10,
+  },
+  {
+    original: slide11,
+    thumbnail: slide11,
+  },
+  {
+    original: slide12,
+    thumbnail: slide12,
+  },
+  {
+    original: slide13,
+    thumbnail: slide13,
+  },
+  {
+    original: slide14,
+    thumbnail: slide14,
+  },
+  {
+    original: slide15,
+    thumbnail: slide15,
+  },
+  {
+    original: slide16,
+    thumbnail: slide16,
+  },
+  {
+    original: slide17,
+    thumbnail: slide17,
+  },
+  {
+    original: slide18,
+    thumbnail: slide18,
+  },
+  {
+    original: slide19,
+    thumbnail: slide19,
+  },
+  
 ];
 
 export default () => {
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,11 +10,14 @@ export default () => {
     <>
     <Titlepage/>
     <div style={{paddingTop: "50px",}}>
-    <p style={{textAlign: "center", fontSize: "25px", fontFamily: "'Playfair Display', serif"}}>
-      For our <span style={{fontWeight: "bold", fontFamily: "Arvo", fontSize: "30px"}}>individual</span> clients, Knot 2 Rustic Woodworks creates <span style={{fontWeight: "bold", fontFamily: "Arvo", fontSize: "30px"}}>unique</span> pieces and installations with great meaning, purpose, and <span style={{fontWeight: "bold", fontFamily: "Arvo", fontSize: "30px"}}>beauty</span> that stand the test of time.  
-      Look through our gallery to discover how Knot 2 Rustic Woodworks creates the perfect piece for any setting.
+    <p style={{textAlign: "center", fontSize: "30px", fontFamily: "'Playfair Display', serif"}}>
+      For our <span style={{fontWeight: "bold", fontFamily: "Arvo", fontSize: "50px"}}>individual</span> clients, Knot 2 Rustic Woodworks creates <span style={{fontWeight: "bold", fontFamily: "Arvo", fontSize: "50px"}}>unique</span> pieces and installations with great meaning, purpose, and <span style={{fontWeight: "bold", fontFamily: "Arvo", fontSize: "50px"}}>beauty</span> that stand the test of time.  
+      Follow along below to discover how Knot 2 Rustic Woodworks creates the <span style={{fontWeight: "bold", fontFamily: "Arvo", fontSize: "50px"}}>perfect</span> piece for any setting.
     </p>
     <Gallery/>
+    <p style={{textAlign: "center", fontSize: "30px", fontFamily: "'Playfair Display', serif"}}>
+      Like any great woodworking design, we consider each of our clients to be unique. Mark and Tony prefer to communicate face to face or over the phone to ensure that each project is held to their high standards. If you are ready to find your perfect piece, click here to contact them. 
+    </p>
     </div>
     </>
   )
